9baad9cdec9323d74695c808627e24aabd29b91d
t <t@t.local>
2026-08-13 00:20:40 +0100

    fix(licence): align library notice with AGPL-3.0-only

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@
 //!
 //! This program is free software: you can redistribute it and/or modify it
 //! under the terms of the GNU Affero General Public License as published by
-//! the Free Software Foundation, either version 3 of the License, or (at your
-//! option) any later version. It is distributed WITHOUT ANY WARRANTY; without
+//! the Free Software Foundation, version 3 of the License. It is distributed
+//! WITHOUT ANY WARRANTY; without
 //! even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 //! PURPOSE. See the GNU Affero General Public License for more details. You
 //! should have received a copy of the License along with this program; if
